8c8701c7cc2d52dbba0cb9a955e20409909b7e23
restorte <vanyakarpenko837@yandex.ru>
2026-03-22 14:22:29 +0300

    mega last fix unit

--- karpenko.ivan/S2/test-queue.cpp
+++ karpenko.ivan/S2/test-queue.cpp
@@ -1,55 +1,83 @@
 #include <boost/test/unit_test.hpp>
 #include "queue.hpp"
 
-using namespace karpenko;
+BOOST_AUTO_TEST_SUITE(QueueTest)
 
-BOOST_AUTO_TEST_SUITE(QueueSuite)
-
-BOOST_AUTO_TEST_CASE(test_push_pop)
+BOOST_AUTO_TEST_CASE(ConstructAndOperatorsTests)
 {
-  Queue<int> q;
-  BOOST_CHECK(q.empty());
-  q.push(10);
-  BOOST_CHECK(!q.empty());
-  BOOST_CHECK_EQUAL(q.front(), 10);
-  BOOST_CHECK_EQUAL(q.back(), 10);
-  q.push(20);
-  BOOST_CHECK_EQUAL(q.front(), 10);
-  BOOST_CHECK_EQUAL(q.back(), 20);
-  q.pop();
-  BOOST_CHECK_EQUAL(q.front(), 20);
-  q.pop();
-  BOOST_CHECK(q.empty());
-}
-
-BOOST_AUTO_TEST_CASE(test_exception_on_empty)
-{
-  Queue<int> q;
-  BOOST_CHECK_THROW(q.pop(), std::out_of_range);
-  BOOST_CHECK_THROW(q.front(), std::out_of_range);
-  BOOST_CHECK_THROW(q.back(), std::out_of_range);
-}
-
-BOOST_AUTO_TEST_CASE(test_copy)
-{
-  Queue<int> q1;
-  q1.push(1);
-  q1.push(2);
-  Queue<int> q2 = q1;
-  BOOST_CHECK_EQUAL(q2.size(), 2);
-  BOOST_CHECK_EQUAL(q2.front(), 1);
-  q2.pop();
-  BOOST_CHECK_EQUAL(q2.front(), 2);
-  BOOST_CHECK_EQUAL(q1.front(), 1);
-}
-
-BOOST_AUTO_TEST_CASE(test_move)
-{
-  Queue<int> q1;
-  q1.push(100);
-  Queue<int> q2 = std::move(q1);
+  karpenko::Queue<int> q1;
   BOOST_CHECK(q1.empty());
-  BOOST_CHECK_EQUAL(q2.front(), 100);
+  q1.push(1);
+  karpenko::Queue<int> q2(q1);
+  BOOST_CHECK(q2.size() == 1);
+  BOOST_CHECK(q2.front() == 1);
+  BOOST_CHECK(q2.back() == 1);
+  q2.push(2);
+  karpenko::Queue<int> q3(std::move(q2));
+  BOOST_CHECK(q3.size() == 2);
+  BOOST_CHECK(q3.front() == 1);
+  BOOST_CHECK(q3.back() == 2);
+  q3.push(3);
+  karpenko::Queue<int> q4 = q3;
+  BOOST_CHECK(q4.size() == 3);
+  BOOST_CHECK(q4.front() == 1);
+  BOOST_CHECK(q4.back() == 3);
+  karpenko::Queue<int> q5 = std::move(q4);
+  BOOST_CHECK(q5.size() == 3);
+  BOOST_CHECK(q5.front() == 1);
+  BOOST_CHECK(q5.back() == 3);
+}
+
+BOOST_AUTO_TEST_CASE(EmptyAndSizeTests)
+{
+  karpenko::Queue<int> q;
+  BOOST_CHECK(q.empty());
+  BOOST_CHECK(q.size() == 0);
+  q.push(1);
+  q.push(2);
+  BOOST_CHECK(!q.empty());
+  BOOST_CHECK(q.size() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(PushPopFrontBackTests)
+{
+  karpenko::Queue<int> q;
+  q.push(1);
+  BOOST_CHECK(q.back() == 1);
+  q.push(2);
+  BOOST_CHECK(q.back() == 2);
+  q.push(3);
+  BOOST_CHECK(q.back() == 3);
+  q.pop();
+  BOOST_CHECK(q.front() == 2);
+  q.pop();
+  BOOST_CHECK(q.front() == 3);
+}
+
+BOOST_AUTO_TEST_CASE(SwapTests)
+{
+  karpenko::Queue<int> q1;
+  q1.push(1);
+  karpenko::Queue<int> q2;
+  q2.push(2);
+  q2.push(3);
+  q2.swap(q1);
+  BOOST_CHECK(q1.front() == 2);
+  BOOST_CHECK(q2.front() == 1);
+  BOOST_CHECK(q1.size() == 2);
+  BOOST_CHECK(q2.size() == 1);
+  BOOST_CHECK(q1.back() == 3);
+}
+
+BOOST_AUTO_TEST_CASE(ClearTests)
+{
+  karpenko::Queue<int> q;
+  q.push(1);
+  q.push(2);
+  q.push(3);
+  BOOST_CHECK(q.size() == 3);
+  q.clear();
+  BOOST_CHECK(q.size() == 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
--- karpenko.ivan/S2/test-stack.cpp
+++ karpenko.ivan/S2/test-stack.cpp
@@ -1,79 +1,78 @@
 #include <boost/test/unit_test.hpp>
 #include "stack.hpp"
 
-using namespace karpenko;
+BOOST_AUTO_TEST_SUITE(StackTest)
 
-BOOST_AUTO_TEST_SUITE(StackSuite)
-
-BOOST_AUTO_TEST_CASE(test_push_pop)
+BOOST_AUTO_TEST_CASE(ConstructAndOperatorsTests)
 {
-  Stack<int> s;
-  BOOST_CHECK(s.empty());
-  s.push(10);
-  BOOST_CHECK(!s.empty());
-  BOOST_CHECK_EQUAL(s.top(), 10);
-  s.push(20);
-  BOOST_CHECK_EQUAL(s.top(), 20);
-  s.pop();
-  BOOST_CHECK_EQUAL(s.top(), 10);
-  s.pop();
-  BOOST_CHECK(s.empty());
-}
-
-BOOST_AUTO_TEST_CASE(test_exception_on_empty)
-{
-  Stack<int> s;
-  BOOST_CHECK_THROW(s.pop(), std::out_of_range);
-  BOOST_CHECK_THROW(s.top(), std::out_of_range);
-}
-
-BOOST_AUTO_TEST_CASE(test_copy)
-{
-  Stack<int> s1;
-  s1.push(1);
-  s1.push(2);
-  Stack<int> s2 = s1;
-  BOOST_CHECK_EQUAL(s2.size(), 2);
-  BOOST_CHECK_EQUAL(s2.top(), 2);
-  s2.pop();
-  BOOST_CHECK_EQUAL(s2.top(), 1);
-  BOOST_CHECK_EQUAL(s1.top(), 2);
-}
-
-BOOST_AUTO_TEST_CASE(test_move)
-{
-  Stack<int> s1;
-  s1.push(100);
-  Stack<int> s2 = std::move(s1);
+  karpenko::Stack<int> s1;
   BOOST_CHECK(s1.empty());
-  BOOST_CHECK_EQUAL(s2.top(), 100);
-}
-
-BOOST_AUTO_TEST_CASE(test_swap)
-{
-  Stack<int> s1;
   s1.push(1);
-  s1.push(2);
-  Stack<int> s2;
-  s2.push(3);
-  s2.push(4);
-  s1.swap(s2);
-  BOOST_CHECK_EQUAL(s1.top(), 4);
-  BOOST_CHECK_EQUAL(s2.top(), 2);
-  BOOST_CHECK_EQUAL(s1.size(), 2);
-  BOOST_CHECK_EQUAL(s2.size(), 2);
+  karpenko::Stack<int> s2(s1);
+  BOOST_CHECK(s2.size() == 1);
+  BOOST_CHECK(s2.top() == 1);
+  s2.push(2);
+  karpenko::Stack<int> s3(std::move(s2));
+  BOOST_CHECK(s3.size() == 2);
+  BOOST_CHECK(s3.top() == 2);
+  s3.push(3);
+  karpenko::Stack<int> s4 = s3;
+  BOOST_CHECK(s4.size() == 3);
+  BOOST_CHECK(s4.top() == 3);
+  karpenko::Stack<int> s5 = std::move(s4);
+  BOOST_CHECK(s5.size() == 3);
+  BOOST_CHECK(s5.top() == 3);
 }
 
-BOOST_AUTO_TEST_CASE(test_clear)
+BOOST_AUTO_TEST_CASE(EmptyAndSizeTests)
 {
-  Stack<int> s;
+  karpenko::Stack<int> s;
+  BOOST_CHECK(s.empty());
+  BOOST_CHECK(s.size() == 0);
+  s.push(1);
+  s.push(2);
+  BOOST_CHECK(!s.empty());
+  BOOST_CHECK(s.size() == 2);
+}
+
+BOOST_AUTO_TEST_CASE(PushPopTopTests)
+{
+  karpenko::Stack<int> s;
+  s.push(1);
+  BOOST_CHECK(s.top() == 1);
+  s.push(2);
+  BOOST_CHECK(s.top() == 2);
+  s.push(3);
+  BOOST_CHECK(s.top() == 3);
+  s.pop();
+  BOOST_CHECK(s.top() == 2);
+  s.pop();
+  BOOST_CHECK(s.top() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(SwapTests)
+{
+  karpenko::Stack<int> s1;
+  s1.push(1);
+  karpenko::Stack<int> s2;
+  s2.push(2);
+  s2.push(3);
+  s2.swap(s1);
+  BOOST_CHECK(s1.top() == 3);
+  BOOST_CHECK(s2.top() == 1);
+  BOOST_CHECK(s1.size() == 2);
+  BOOST_CHECK(s2.size() == 1);
+}
+
+BOOST_AUTO_TEST_CASE(ClearTests)
+{
+  karpenko::Stack<int> s;
   s.push(1);
   s.push(2);
   s.push(3);
-  BOOST_CHECK_EQUAL(s.size(), 3);
+  BOOST_CHECK(s.size() == 3);
   s.clear();
-  BOOST_CHECK(s.empty());
-  BOOST_CHECK_EQUAL(s.size(), 0);
+  BOOST_CHECK(s.size() == 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
