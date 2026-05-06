3c689147405a70de3b2a3651925ba8f14e029b2a
restorte <vanyakarpenko837@yandex.ru>
2026-05-06 19:47:55 +0300

    feat(Vector): implement constructors and pushBack

--- karpenko.ivan/common/hash_table.hpp
+++ karpenko.ivan/common/hash_table.hpp
@@ -79,6 +79,219 @@ private:
   void constructFromRange(size_t start, IT begin, IT end);
 };
 
+// ── реализация конструкторов / деструктора / pushBack ──
+
+template <class T>
+Vector<T>::Vector() : data_(nullptr), size_(0), capacity_(0)
+{
+}
+
+template <class T>
+Vector<T>::Vector(size_t size, const T& val)
+  : data_(nullptr), size_(size), capacity_(size)
+{
+  if (size_ == 0) return;
+  data_ = static_cast<T*>(::operator new(sizeof(T) * size_));
+  for (size_t i = 0; i < size_; ++i)
+  {
+    new (data_ + i) T(val);
+  }
+}
+
+template <class T>
+Vector<T>::Vector(size_t size)
+  : data_(nullptr), size_(0), capacity_(0)
+{
+  reserve(size);
+}
+
+template <class T>
+Vector<T>::Vector(std::initializer_list<T> il)
+  : data_(nullptr), size_(0), capacity_(0)
+{
+  reserve(il.size());
+  size_ = il.size();
+  size_t i = 0;
+  for (auto it = il.begin(); it != il.end(); ++it, ++i)
+  {
+    new (data_ + i) T(*it);
+  }
+}
+
+template <class T>
+Vector<T>::Vector(Vector<T>&& rhs) noexcept
+  : data_(rhs.data_), size_(rhs.size_), capacity_(rhs.capacity_)
+{
+  rhs.data_ = nullptr;
+  rhs.size_ = 0;
+  rhs.capacity_ = 0;
+}
+
+template <class T>
+Vector<T>& Vector<T>::operator=(Vector<T>&& rhs) noexcept
+{
+  if (this != &rhs)
+  {
+    destroyRange(0, size_);
+    deallocate();
+    data_ = rhs.data_;
+    size_ = rhs.size_;
+    capacity_ = rhs.capacity_;
+    rhs.data_ = nullptr;
+    rhs.size_ = 0;
+    rhs.capacity_ = 0;
+  }
+  return *this;
+}
+
+template <class T>
+Vector<T>::Vector(const Vector<T>& rhs)
+  : data_(nullptr), size_(0), capacity_(0)
+{
+  reserve(rhs.size_);
+  size_ = rhs.size_;
+  for (size_t i = 0; i < size_; ++i)
+  {
+    new (data_ + i) T(rhs.data_[i]);
+  }
+}
+
+template <class T>
+Vector<T>& Vector<T>::operator=(const Vector<T>& rhs)
+{
+  if (this != &rhs)
+  {
+    Vector<T> cpy(rhs);
+    swap(cpy);
+  }
+  return *this;
+}
+
+template <class T>
+Vector<T>::~Vector()
+{
+  destroyRange(0, size_);
+  deallocate();
+}
+
+template <class T>
+void Vector<T>::pushBack(const T& val)
+{
+  if (size_ == capacity_)
+  {
+    size_t newCapacity = capacity_ == 0 ? 1 : capacity_ * 2;
+    reserve(newCapacity);
+  }
+  new (data_ + size_) T(val);
+  ++size_;
+}
+
+template <class T>
+void Vector<T>::reserve(size_t k)
+{
+  if (k <= capacity_) return;
+  T* newData = static_cast<T*>(::operator new(sizeof(T) * k));
+  for (size_t i = 0; i < size_; ++i)
+  {
+    new (newData + i) T(std::move(data_[i]));
+    data_[i].~T();
+  }
+  deallocate();
+  data_ = newData;
+  capacity_ = k;
+}
+
+template <class T>
+void Vector<T>::shrinkToFit()
+{
+  if (size_ == capacity_) return;
+  if (size_ == 0)
+  {
+    destroyRange(0, size_);
+    deallocate();
+    data_ = nullptr;
+    capacity_ = 0;
+    return;
+  }
+  T* newData = static_cast<T*>(::operator new(sizeof(T) * size_));
+  for (size_t i = 0; i < size_; ++i)
+  {
+    new (newData + i) T(std::move(data_[i]));
+    data_[i].~T();
+  }
+  deallocate();
+  data_ = newData;
+  capacity_ = size_;
+}
+
+template <class T>
+void Vector<T>::repeatPushBack(const T& val, size_t k)
+{
+  if (k == 0) return;
+  reserve(size_ + k);
+  for (size_t i = 0; i < k; ++i)
+  {
+    new (data_ + size_ + i) T(val);
+  }
+  size_ += k;
+}
+
+template <class T>
+template <class IT>
+void Vector<T>::rangedPushBack(IT beg, size_t count)
+{
+  if (count == 0) return;
+  reserve(size_ + count);
+  for (size_t i = 0; i < count; ++i, ++beg)
+  {
+    new (data_ + size_ + i) T(*beg);
+  }
+  size_ += count;
+}
+
+template <class T>
+void Vector<T>::repeatInsert(size_t id, const T& val, size_t k)
+{
+  if (id > size_) throw std::out_of_range("Vector::repeatInsert: position out of range");
+  if (k == 0) return;
+  T* newData = static_cast<T*>(::operator new(sizeof(T) * (size_ + k)));
+  for (size_t i = 0; i < id; ++i)
+  {
+    new (newData + i) T(std::move(data_[i]));
+    data_[i].~T();
+  }
+  for (size_t i = 0; i < k; ++i)
+  {
+    new (newData + id + i) T(val);
+  }
+  for (size_t i = id; i < size_; ++i)
+  {
+    new (newData + i + k) T(std::move(data_[i]));
+    data_[i].~T();
+  }
+  deallocate();
+  data_ = newData;
+  size_ += k;
+  capacity_ = size_ + k;
+}
+
+template <class T>
+bool Vector<T>::operator==(const Vector<T>& rhs) const noexcept
+{
+  if (size_ != rhs.size_) return false;
+  for (size_t i = 0; i < size_; ++i)
+  {
+    if (data_[i] != rhs.data_[i]) return false;
+  }
+  return true;
+}
+
+template <class T>
+bool Vector<T>::operator!=(const Vector<T>& rhs) const noexcept
+{
+  return !(*this == rhs);
+}
+
 }
 
 #endif
