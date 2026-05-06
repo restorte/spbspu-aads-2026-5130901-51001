b728c98a68bbf64ff6036910798f74df47721167
restorte <vanyakarpenko837@yandex.ru>
2026-05-06 20:50:06 +0300

    fix

--- hachaturyanov.ivan/S0/credentials.cpp
+++
@@ -1,7 +0,0 @@
-#include "credentials.hpp"
-#include <iostream>
-
-std::ostream & hachaturyanov::out_credentials(std::ostream &out)
-{
-  return out << "hachaturyanov.ivan";
-}
--- hachaturyanov.ivan/S0/main.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-#include "credentials.hpp"
-
-int main()
-{
-  hachaturyanov::out_credentials(std::cout) << "\n";
-}
