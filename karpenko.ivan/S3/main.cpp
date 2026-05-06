fee34aeb7a3932f842d7c24c15a76afb3281615c
restorte <vanyakarpenko837@yandex.ru>
2026-05-06 20:35:33 +0300

    feat: main complete

--- karpenko.ivan/S3/main.cpp
+++ karpenko.ivan/S3/main.cpp
@@ -1,3 +1,28 @@
-int main()
+#include <iostream>
+
+#include "command_system.hpp"
+
+int main(int argc, char* argv[])
 {
+    if (argc != 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " <filename>\n";
+        return 1;
+    }
+    try
+    {
+        karpenko::GraphCollection graphs = karpenko::readGraphs(argv[1]);
+        karpenko::CommandSystem cs;
+        std::string line;
+        while (std::getline(std::cin, line))
+        {
+            cs.execute(line, std::cout, graphs);
+        }
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+        return 1;
+    }
+    return 0;
 }
