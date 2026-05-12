e2c2e4c255a3a0e25109af759625a8b8c1d42824
restorte <vanyakarpenko837@yandex.ru>
2026-05-12 10:28:40 +0300

    add TreeNode structure

---
+++ karpenko.ivan/common/tree_node.hpp
@@ -0,0 +1,24 @@
+#ifndef KARPENKO_TREE_NODE_HPP
+#define KARPENKO_TREE_NODE_HPP
+
+#include <utility>
+
+namespace karpenko {
+
+template <typename Key, typename Value>
+struct TreeNode {
+    std::pair<const Key, Value> data;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode* parent;
+
+    TreeNode(const Key& k, Value v)
+        : data(k, std::move(v)), left(nullptr), right(nullptr), parent(nullptr) {}
+
+    TreeNode()
+        : data(Key(), Value()), left(nullptr), right(nullptr), parent(nullptr) {}
+};
+
+}
+
+#endif
