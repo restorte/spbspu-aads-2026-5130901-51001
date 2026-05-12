180bce916fbde3be963184f7d86b20db4d92d151
restorte <vanyakarpenko837@yandex.ru>
2026-05-12 10:38:10 +0300

    add initial parser tests

---
+++ karpenko.ivan/S4/test-parser.cpp
@@ -0,0 +1,31 @@
+#define BOOST_TEST_MODULE test_parser
+#include <boost/test/included/unit_test.hpp>
+#include <string>
+#include "../common/bstree.hpp"
+
+BOOST_AUTO_TEST_CASE(split_function) {
+    std::string* tokens = nullptr;
+    size_t count = 0;
+
+    karpenko::split("  print first ", tokens, count);
+    BOOST_REQUIRE_EQUAL(count, 2);
+    BOOST_TEST(tokens[0] == "print");
+    BOOST_TEST(tokens[1] == "first");
+    delete[] tokens;
+
+    karpenko::split("   ", tokens, count);
+    BOOST_TEST(count == 0);
+    delete[] tokens;
+
+    karpenko::split("hello", tokens, count);
+    BOOST_REQUIRE_EQUAL(count, 1);
+    BOOST_TEST(tokens[0] == "hello");
+    delete[] tokens;
+
+    karpenko::split(" one  two   three ", tokens, count);
+    BOOST_REQUIRE_EQUAL(count, 3);
+    BOOST_TEST(tokens[0] == "one");
+    BOOST_TEST(tokens[1] == "two");
+    BOOST_TEST(tokens[2] == "three");
+    delete[] tokens;
+}
